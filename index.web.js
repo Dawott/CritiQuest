2ceb79c794f235ee1b2908dda627b7af47bbea3b
Dawott <dawid.tott@gmail.com>
2025-06-20 10:25:44 +0200

    Dependencies for web

---
+++ index.web.js
@@ -0,0 +1,7 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('CritiQuest', () => App);
+AppRegistry.runApplication('CritiQuest', {
+  rootTag: document.getElementById('root')
+});
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.web.js',
+  mode: 'development',
+  devServer: {
+    port: 3000,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web',
+      'react-native-vector-icons': 'react-native-vector-icons/dist'
+    },
+    extensions: ['.web.js', '.js', '.jsx', '.ts', '.tsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
